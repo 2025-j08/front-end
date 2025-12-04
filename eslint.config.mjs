d8cb15102b44e83fdc1fad38c4d69a5a002632b0
yuki-kobashi <kd1344505@st.kobedenshi.ac.jp>
2025-12-04 14:13:07 +0900

    lintを設定

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,46 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+// eslint.config.mjs
+import nextConfig from "eslint-config-next";
+import importPlugin from "eslint-plugin-import";
+import { defineConfig } from "eslint/config";
+import prettierConfig from "eslint-config-prettier";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+export default defineConfig([
+  // Next.js 公式の Flat Config を丸ごと採用
+  ...nextConfig,
+
+  // 自作ルール、追加プラグイン
+  {
+    plugins: {
+      import: importPlugin,
+    },
+
+    rules: {
+      // import 順
+      "import/order": [
+        "warn",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling", "index"],
+          ],
+          "newlines-between": "always",
+        },
+      ],
+    },
+  },
+
+  // prettier を一番最後に当てる（競合回避）
+  prettierConfig,
+
+  // 無視パターン
+  {
+    ignores: [
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
 ]);
-
-export default eslintConfig;
