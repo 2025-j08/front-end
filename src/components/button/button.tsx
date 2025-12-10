129e1837ea9a71076314931be62b692a3e94138b
pci873 <150927769+pci873@users.noreply.github.com>
2025-12-10 14:28:22 +0900

    ボタンコンポーネントでtype属性を指定できるようpropsを変更

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -2,9 +2,13 @@ import React, { MouseEventHandler, ReactNode } from "react";
 
 import styles from './button.module.scss';
 
-export default function Button(props: { children: ReactNode, onClick?: MouseEventHandler }) {
+export default function Button(props: { children: ReactNode, onClick?: MouseEventHandler, type?: 'button' | 'submit' | 'reset' }) {
     return (
-        <button className={styles.button} onClick={props.onClick}>
+        <button
+            className={styles.button}
+            onClick={props.onClick}
+            type={props.type ?? 'button'}
+        >
             {props.children}
         </button>
     )
