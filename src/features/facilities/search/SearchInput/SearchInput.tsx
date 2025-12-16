b67fc5ba95b7abb3ea7b34f56685f44f6659a8ba
kotahyodo <kd1379529@st.kobedenshi.ac.jp>
2025-12-16 13:56:27 +0900

    キーワード検索UI

---
+++ src/features/facilities/search/SearchInput/SearchInput.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styles from './SearchInput.module.scss';
+
+export const SearchInput = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.inputWrapper}>
+        <input type="text" placeholder="キーワードを入力........." className={styles.input} />
+        <button className={styles.searchButton}>検索</button>
+      </div>
+    </div>
+  );
+};
