cb29b0a215531d67d062f715036aab3c5a0f9205
shoma-nogawa <kd1348617@st.kobedenshi.ac.jp>
2026-01-06 14:01:57 +0900

    Revert "Merge pull request #27 from 2025-j08/login-shoma"
    This reverts commit 94972b3a10a71cde3a00724b1c99a2e4fdac879c, reversing
    changes made to d3f95a08439a35e6cb79f53a825aff5474d89386.

---
+++ src/app/features/contact/page.tsx
@@ -0,0 +1,9 @@
+/**
+ * /features/contact ルート
+ * 新しいcontactFormコンポーネントを使用
+ */
+import { ContactForm } from '@/features/contact/contactForm';
+
+export default function FeaturesContactPage() {
+  return <ContactForm />;
+}
