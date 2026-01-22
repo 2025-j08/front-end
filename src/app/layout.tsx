94b10a1acafbcdb397077247e22a8c1a385ce364
yuki-kobashi <kd1344505@st.kobedenshi.ac.jp>
2026-01-22 11:49:36 +0900

    feat: Google Tag Manager (GTM) を導入
    - layout.tsx に GTM スクリプトを追加
    - 環境変数 NEXT_PUBLIC_GTM_ID で制御可能に
    - html lang を ja に修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import Script from 'next/script';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v16-appRouter';
 
 import './globals.css';
@@ -7,6 +8,8 @@ import './globals.css';
 import { Header } from '@/components/header/header';
 import { Footer } from '@/components/footer/footer';
 import { siteMetadata } from '@/lib/metadata';
+
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,7 +31,33 @@ export default function RootLayout({
   return (
     <AppRouterCacheProvider>
       <html lang="ja">
+        {GTM_ID && (
+          <Script
+            id="gtm-script"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${GTM_ID}');
+              `,
+            }}
+          />
+        )}
         <body className={`${geistSans.variable} ${geistMono.variable}`}>
+          {/* Google Tag Manager (noscript) */}
+          {GTM_ID && (
+            <noscript>
+              <iframe
+                src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+                height="0"
+                width="0"
+                style={{ display: 'none', visibility: 'hidden' }}
+              />
+            </noscript>
+          )}
           {/* ヘッダー 一番上固定 */}
           <Header />
 
